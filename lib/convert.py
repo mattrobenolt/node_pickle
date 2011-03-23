9aa1e9d94bc348f4574812fb522d89e97cf81b9a
Matt Robenolt <matt@ydekproductions.com>
2011-03-23 19:05:06 -0400

    first commit

---
+++ lib/convert.py
@@ -0,0 +1,18 @@
+import sys
+import simplejson as json
+try:
+    import cPickle as pickle
+except ImportError:
+    import pickle
+
+def main(argv):
+    try:
+        if argv[0] == '--loads':
+            json.dump(pickle.load(sys.stdin), sys.stdout)
+        elif argv[0] == '--dumps':
+            pickle.dump(json.load(sys.stdin), sys.stdout)
+    except:
+        sys.stdout.write('-1')
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
